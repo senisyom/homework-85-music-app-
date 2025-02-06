bbab4a192ac7a9c3bba39b62df203e908214ec19
Begmatov Almaz <lolkek13@inbox.ru>
2025-02-06 15:08:59 +0600

    Added google route in backend

--- api/routers/user.ts
+++ api/routers/user.ts
@@ -1,76 +1,126 @@
 import express from "express";
 import User from "../models/User";
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
+import { OAuth2Client } from "google-auth-library";
+import config from "../config";
+import { imagesUpload } from "../multer";
 
 const userRouter = express.Router();
+const googleClient = new OAuth2Client(config.google.clientId);
 
-userRouter.post('/', async (req, res, next) => {
-    try{
+userRouter.post("/", imagesUpload.single("avatar"), async (req, res, next) => {
+  try {
+    const userMutation = {
+      username: req.body.username,
+      password: req.body.password,
+      displayName: req.body.displayName,
+      avatar: req.file ? req.file.filename : null,
+    };
 
-        const user = new User({
-            username: req.body.username,
-            password: req.body.password,
-        });
+    console.log(userMutation);
 
-        user.generateToken();
-        await user.save();
-        return res.send(user);
-    } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
-            return res.status(400).send(error);
-        }
+    const user = new User(userMutation);
 
-        return next(error);
+    user.generateToken();
+    await user.save();
+    return res.send(user);
+  } catch (error) {
+    if (error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
     }
+
+    return next(error);
+  }
 });
 
+userRouter.post("/google", async (req, res, next) => {
+  try {
+    const ticket = await googleClient.verifyIdToken({
+      idToken: req.body.credential,
+      audience: config.google.clientId,
+    });
 
+    const payload = ticket.getPayload();
 
-userRouter.post('/sessions', async (req, res, next) => {
-    try {
-        const user = await User.findOne({username: req.body.username});
-        
-        if (!user) {
-            return res.status(400).send({error: 'Username not found!'});
-        }
-        
-        const isMatch = await user.checkPassword(req.body.password);
-        
-        if (!isMatch) {
-            return res.status(400).send({error: 'Password is wrong!'});
-        }
-        
-        user.generateToken();
-        await user.save();
-        
-        return res.send(user);
-    } catch (error) {
-        return next(error);
+    if (!payload) {
+      return res.status(400).send({ error: "Google Login Error!" });
     }
+
+    const email = payload.email;
+    const id = payload.sub;
+    const displayName = payload.name;
+
+    if (!email) {
+      return res
+        .status(400)
+        .send({ error: "Not enough user data to continue!" });
+    }
+
+    let user = await User.findOne({ googleId: id });
+
+    if (!user) {
+      const newPassword = crypto.randomUUID();
+      user = new User({
+        username: email,
+        password: newPassword,
+        googleId: id,
+        displayName,
+        avatar: payload.picture,
+      });
+    }
+
+    user.generateToken();
+    await user.save();
+    return res.send(user);
+  } catch (error) {
+    return next(error);
+  }
 });
 
-userRouter.delete('/sessions', async (req, res, next) => {
-    try {
-      const headerValue = req.get('Authorization');
-  
-      if (!headerValue) return res.status(204).send();
-  
-      const [_bearer, token] = headerValue.split(' ');
-  
-      if (!token) return res.status(204).send();
-  
-      const user = await User.findOne({token});
-  
-      if (!user) return res.status(204).send();
-  
-      user.generateToken();
-      await user.save();
-  
-      return res.status(204).send();
-    } catch (error) {
-      return next(error);
+userRouter.post("/sessions", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(400).send({ error: "Username not found!" });
     }
-  });
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(400).send({ error: "Password is wrong!" });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send(user);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userRouter.delete("/sessions", async (req, res, next) => {
+  try {
+    const headerValue = req.get("Authorization");
+
+    if (!headerValue) return res.status(204).send();
+
+    const [_bearer, token] = headerValue.split(" ");
+
+    if (!token) return res.status(204).send();
+
+    const user = await User.findOne({ token });
+
+    if (!user) return res.status(204).send();
+
+    user.generateToken();
+    await user.save();
+
+    return res.status(204).send();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default userRouter;
--- api/types.ts
+++ api/types.ts
@@ -1,48 +1,51 @@
-import {Model, ObjectId} from "mongoose";
+import { Model, ObjectId } from "mongoose";
 
 export interface ArtistMutation {
-    name: string;
-    description: string | null;
-    image: string | null;
+  name: string;
+  description: string | null;
+  image: string | null;
 }
 
 export interface AlbumMutation {
-    name: string;
-    artist: ObjectId;
-    date: Date;
-    image: string | null;
-    user: ObjectId | undefined;
-    isPublished: boolean;
+  name: string;
+  artist: ObjectId;
+  date: Date;
+  image: string | null;
+  user: ObjectId | undefined;
+  isPublished: boolean;
 }
 
 export interface ITrack {
-    _id: ObjectId;
-    name: string;
-    album: ObjectId;
-    duration: string;
-    trackNumber: number;
-    isPublished: boolean;
+  _id: ObjectId;
+  name: string;
+  album: ObjectId;
+  duration: string;
+  trackNumber: number;
+  isPublished: boolean;
 }
 
-export type TrackMutation = Omit<ITrack, '_id'>;
+export type TrackMutation = Omit<ITrack, "_id">;
 
 export interface UserFields {
-    username: string;
-    password: string;
-    token: string;
-    role: string;
+  username: string;
+  password: string;
+  token: string;
+  role: string;
+  googleId?: string;
+  displayName: string;
+  avatar: string | null;
 }
 
 export interface UserMethods {
-    checkPassword(password: string): Promise<boolean>;
-    generateToken(): void;
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
 }
 
 export type UserModel = Model<UserFields, {}, UserMethods>;
 
 export interface ITrackHistory {
-    _id: ObjectId;
-    user: ObjectId;
-    track: ObjectId;
-    date: string;
+  _id: ObjectId;
+  user: ObjectId;
+  track: ObjectId;
+  date: string;
 }
